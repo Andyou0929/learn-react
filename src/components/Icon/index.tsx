2af46d48ea9f3441c0dd0b21fed5b69d8acd21cb
anxia.ack <anchenkai@come-future.com>
2024-11-21 23:51:40 +0800

    feat:🖥  添加icon组件

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,7 @@
+import { createFromIconfontCN } from "@ant-design/icons";
+
+const IconFont = createFromIconfontCN({
+  scriptUrl:"//at.alicdn.com/t/c/font_4755159_sy3zmtflhvi.js"
+})
+
+export default IconFont
